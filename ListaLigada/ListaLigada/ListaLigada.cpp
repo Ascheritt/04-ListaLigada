eb6dec655ca2613ce482b96154c81ee347cbba01
lucas amaral <lucas.amaral.conta@gmail.com>
2023-03-21 15:04:20 -0300

    Alterações no inserir elemento, criação do excluir elemento e buscar elemento.

--- ListaLigada/ListaLigada/ListaLigada.cpp
+++ ListaLigada/ListaLigada/ListaLigada.cpp
@@ -128,11 +128,17 @@ void inserirElemento()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
+	/*while (posicaoElemento(novo->valor) != NULL) {
+		cout << "Digite um elemento que nao esteja na lista:" << endl;
+		cin >> novo->valor;
+	}
+	*/
+
 	if (primeiro == NULL)
 	{
 		primeiro = novo;
 	}
-	else
+	else if (posicaoElemento(novo->valor) == NULL)
 	{
 		// procura o final da lista
 		NO* aux = primeiro;
@@ -141,16 +147,52 @@ void inserirElemento()
 		}
 		aux->prox = novo;
 	}
+	else {
+		cout << "Elemento já esta na lista" << endl;
+	}
 }
 
 void excluirElemento()
 {
-	
+	int num;
+	cout << "Digite o elemento que deseja excluir:";
+	cin >> num;
+
+	if (posicaoElemento(num) == NULL) {
+		cout << "ELEMENTO NAO ENCONTRADO" << endl;
+	}
+	else {
+		NO* aux = primeiro;
+		NO* nd = posicaoElemento(num);
+
+		if (nd == primeiro) {
+			primeiro = primeiro->prox;
+		}
+		else {
+
+			while (aux->prox != nd) {
+				aux = aux->prox;
+			}
+
+			aux->prox = nd->prox;
+			free(nd);
+		}
+	}
 }
 
 void buscarElemento()
 {
-	
+	int num;
+	cout << "Digite o elemento a ser buscado:";
+	cin >> num;
+	if (posicaoElemento(num) != NULL) {
+		cout << "ENCONTRADO" << endl;
+	}
+	else {
+		cout << "ELEMENTO NAO ENCONTRADO" << endl;
+	}
+
+
 }
 
 
